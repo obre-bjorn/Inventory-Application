060b278980e1ca609cba38b15b340f5ee7a9906e
obre-bjorn <obre.bjorn@gmail.com>
2024-05-28 15:11:33 +0300

    initialized project and set up the requirements and dependencies

---
+++ routes/inventory.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router()
+
+
+router.get('/', (req,res) =>{
+    res.send('This is the Inventory listing page')
+})
+
+
+module.exports = router
+
+
+
